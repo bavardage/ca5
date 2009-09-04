a113f844141d653bb5eb50e2598282e51af074ce
Ben Duffield <jebavarde@gmail.com>
2009-09-04 20:03:33 +0100

    Tidy up/modularise interface code.

--- ca5.js
+++ ca5.js
@@ -1,164 +1,79 @@
+simulations = [];
+grid = new Grid(10, 10);
+
+function Simulation(name, description, setup) {
+    this.name = name;
+    this.description = description;
+    this.setup = setup;
+    simulations.push(this);
+}
+
 function setupInterface() {
-    debug('initialising interface');
-    g = new Grid(10, 10);
-    $('#tick').click(function() {
-	    g.tick();
-	    debug('ticked grid');
-	    $('#canvas').each(function() {
-		    debug('drawing ' + g + ' a canvas' + this);
-		    g.drawToCanvas(this);
-		});
-	    debug('all done');
+    simulations.forEach(function(sim) {
+	    $('#simulations').append("<li><a href='#' onClick='loadSim(\""
+				     + sim.name + "\")'>" 
+				     + sim.name 
+				     + "</a></li>");
 	});
-    
-    $('#test1').click(function() {
-	    if(!g.running) colourTest1();
+    $('#controls a').click(function() {
+	    debug('playpause');
 	    $('#canvas').each(function() {
-		    debug('toggling play');
-		    g.togglePlay(this);
-		});
-	});
-    $('#test2').click(function() {
-	    if(!g.running) colourTest2();
-	    $('#canvas').each(function() {
-		    debug('toggling play');
-		    g.togglePlay(this);
-		});
-	});
-
-    $('#lifetest').click(function() {
-	    if(!g.running) lifeTest();
-	    $('#canvas').each(function() {
-		    debug('toggling play');
-		    g.togglePlay(this);
-		});
-	});
-
-    $('#glidertest').click(function() {
-	    if(!g.running) gliderTest();
-	    $('#canvas').each(function() {
-		    debug('toggling play');
-		    g.togglePlay(this);
+		    grid.togglePlay(this);
 		});
 	});
 }
 
-function colourTest1() {
-    g.width = 20; g.height = 20;
-    g.reset();
-    c = new FunkyCell(g, 2, 2);
-    new CopyCell(g, 2, 3, c);
-    new CopyCell(g, 4, 2, c);
-    new CopyCell(g, 3, 4, c);
-
-    c = new FunkyCell(g, 15, 4);
-    c.fillColor = new Color(0, 255, 0);
-    new CopyCell(g, 15, 5, c);
-    new CopyCell(g, 16, 4, c);
-    new CopyCell(g, 16, 5, c);
-
-    c = new FunkyCell(g, 5, 18);
-    c.fillColor = new Color(0, 0, 255);
-    c = new CopyCell(g, 5, 17, c);
-    c = new CopyCell(g, 5, 16, c);
-    c = new CopyCell(g, 5, 15, c);
-
-    c = new FunkyCell(g, 10, 10);
-    c.fillColor = new Color(255, 0, 255);
-    
-    c = new FunkyCell(g, 19, 17);
-    c.fillColor = new Color(0, 255, 255);
-
-    for(var i = 0; i < g.width; i++)
-	for(var j = 0; j < g.height; j++)
-	    new ChamCell(g, i, j);
-}
-
-function colourTest2() {
-    debug('colourTest2');
-
-
-    g.width = 40;
-    g.height = 40;
-    g.reset();
-    debug('resized');
-
-    new FunkyCell(g, 2, 2);
-    debug('cell made');
-    new FunkyCell(g, 10, 2);
-    debug('cell made');
-    new FunkyCell(g, 17, 2);
-    debug('cell made');
-    new FunkyCell(g, 5, 3);
-    debug('cell made');
-    new FunkyCell(g, 5, 9);
-    debug('cell made');
-    new FunkyCell(g, 7, 18);
-    debug('cell made');
-    new FunkyCell(g, 15, 1);
-    debug('cell made');
-    new FunkyCell(g, 15, 11);
-    debug('cell made');
-    new FunkyCell(g, 15, 19);
-    debug('funkycells made');
-
-    for(var i = 0; i < g.width; i++)
-	for(var j = 0; j < g.height; j++)
-	    new ChamCell(g, i, j);
-}
-
-function lifeTest() {
-    debug('lifetest');
-    g.width = 30;
-    g.height = 30;
-    g.reset();
-
-    for(var i = 0; i < g.width; i++)
-	for(var j = 0; j < g.height; j++) {
-	    c = new GOLCell(g, i, j);
-	    if(Math.random() > 0.7) {
-		debug('making an alive cell');
-		c.nextAlive = true;
-		c.tock();
-	    }
+function loadSim(name) {
+    debug('loading sim');
+    len = simulations.length;
+    for(var i = 0; i < len; i++)
+	if(simulations[i].name == name) {
+	    debug('sim is: ' + simulations[i].description);
+	    simulations[i].setup();
 	}
-    $('#canvas').each(function() {
-	    g.drawToCanvas(this);
-	});
 }
 
-function gliderTest() {
-    debug('glidertest');
-    g.width = 20;
-    g.height = 20;
-    g.reset();
+new Simulation("Pretty Colours",
+	       "Displays some randomly changing colours and stuff.",
+	       function() {
+		   grid.width = 10; grid.height = 10;
+		   grid.reset();
+		   c = new FunkyCell(grid, 2, 2);
+		   new CopyCell(grid, 2, 3, c);
+		   new CopyCell(grid, 4, 2, c);
+		   new CopyCell(grid, 3, 4, c);
+		   grid.forEach(function(x, y) {
+			   new ChamCell(grid, x, y);
+		       });
+});
 
-    c = new GOLCell(g, 5, 5);
-    c.nextAlive = true;
-    c.tock();
-    c = new GOLCell(g, 5, 6);
-    c.nextAlive = true;
-    c.tock();
-    c = new GOLCell(g, 6, 6);
-    c.nextAlive = true;
-    c.tock();
-    c = new GOLCell(g, 4, 7);
-    c.nextAlive = true;
-    c.tock();
-    c = new GOLCell(g, 6, 7);
-    c.nextAlive = true;
-    c.tock();
+new Simulation("Life",
+	       "Conway's game of life",
+	       function() {
+		   grid.width = 30;
+		   grid.height = 30;
+		   grid.reset();
+		   grid.forEach(function(x,y) {
+			   var alive = false;
+			   if(Math.random() > 0.7)
+			       alive = true;
+			   c = new GOLCell(grid, x, y, alive);
+		       });
+});
 
-    for(var i = 0; i < g.width; i++)
-	for(var j = 0; j < g.width; j++)
-	    if(!g.cells[i][j].length)
-		new GOLCell(g, i, j);
-    debug('made cells');
-    
-
-
-    $('#canvas').each(function() {
-	    g.drawToCanvas(this);
-	});
-    
-}
+new Simulation("Glider",
+	       "Conway's game of life with a single glider",
+	       function() {
+		   grid.width = 20; grid.height = 20;
+		   grid.reset();
+		   new GOLCell(grid, 5, 5, true);
+		   new GOLCell(grid, 5, 6, true);
+		   new GOLCell(grid, 6, 6, true);
+		   new GOLCell(grid, 4, 7, true);
+		   new GOLCell(grid, 6, 7, true);
+		   grid.forEach(function(x,y) {
+			   if(!grid.cells[x][y].length)
+			       new GOLCell(grid, x, y);
+		       });
+});
+		
